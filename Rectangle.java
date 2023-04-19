59abfea21b18931ea3c8a21fa18a04d3f04c9f13
mzqan <123510256+mzqan@users.noreply.github.com>
2023-04-19 08:33:11 -0400

    Created Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle {
+    private int width;
+    private int lenght;
+    
+    public Rectangle(int theWidth, int theLength) {
+        width = theWidth;
+        length = theLength;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getLength() {
+        return length;
+    }
+    
+    public String toString()
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+    
+}+
